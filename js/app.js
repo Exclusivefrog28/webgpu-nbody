b608463319a25610d20e5b071ac5ff137056574e
excf28 <deee4444@gmail.com>
2024-07-08 14:51:53 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,118 @@
+const loadShader = async () => {
+  let shaderCode = await fetch('shader/matrix.wgsl');
+  return await shaderCode.text()
+}
+
+(async () => {
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    return;
+  }
+  const device = await adapter.requestDevice();
+
+  // First Matrix
+
+  const firstMatrix = new Float32Array([
+    0, 0, 0, 0, 1,
+    2, 2, 0, 0, 1
+  ]);
+
+  const gpuBufferFirstMatrix = device.createBuffer({
+    mappedAtCreation: true,
+    size: firstMatrix.byteLength,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST,
+  });
+  const arrayBufferFirstMatrix = gpuBufferFirstMatrix.getMappedRange();
+  new Float32Array(arrayBufferFirstMatrix).set(firstMatrix);
+  gpuBufferFirstMatrix.unmap();
+
+
+  // Second Matrix
+
+  const secondMatrix = new Float32Array([
+    0, 0, 0, 0, 1,
+    2000000, 2000000, 0, 0, 1
+  ]);
+
+  const gpuBufferSecondMatrix = device.createBuffer({
+    mappedAtCreation: true,
+    size: secondMatrix.byteLength,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST,
+  });
+  const arrayBufferSecondMatrix = gpuBufferSecondMatrix.getMappedRange();
+  new Float32Array(arrayBufferSecondMatrix).set(secondMatrix);
+  gpuBufferSecondMatrix.unmap();
+
+
+  const shaderModule = device.createShaderModule({
+    code: await loadShader()
+  });
+
+  const computePipeline = device.createComputePipeline({
+    layout: "auto",
+    compute: {
+      module: shaderModule,
+      entryPoint: "main"
+    }
+  });
+
+  const bindGroup = device.createBindGroup({
+    layout: computePipeline.getBindGroupLayout(0 /* index */),
+    entries: [
+      {
+        binding: 0,
+        resource: {
+          buffer: gpuBufferFirstMatrix
+        }
+      },
+      {
+        binding: 1,
+        resource: {
+          buffer: gpuBufferSecondMatrix
+        }
+      }
+    ]
+  });
+
+  const commandEncoder = device.createCommandEncoder();
+
+  const passEncoder = commandEncoder.beginComputePass();
+  passEncoder.setPipeline(computePipeline);
+  passEncoder.setBindGroup(0, bindGroup);
+  const workgroupCount = Math.ceil((firstMatrix.length / 5) / 8);
+  passEncoder.dispatchWorkgroups(workgroupCount);
+  passEncoder.end();
+
+  // Get a GPU buffer for reading in an unmapped state.
+  const gpuReadBuffer = device.createBuffer({
+    size: secondMatrix.byteLength,
+    usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
+  });
+
+  // Encode commands for copying buffer to buffer.
+  commandEncoder.copyBufferToBuffer(
+    gpuBufferSecondMatrix /* source buffer */,
+    0 /* source offset */,
+    gpuBufferFirstMatrix /* destination buffer */,
+    0 /* destination offset */,
+    secondMatrix.byteLength /* size */
+  );
+  commandEncoder.copyBufferToBuffer(
+    gpuBufferSecondMatrix /* source buffer */,
+    0 /* source offset */,
+    gpuReadBuffer /* destination buffer */,
+    0 /* destination offset */,
+    secondMatrix.byteLength /* size */
+  );
+
+  // Submit GPU commands.
+  const gpuCommands = commandEncoder.finish();
+  device.queue.submit([gpuCommands]);
+
+  // Read buffer.
+  await gpuReadBuffer.mapAsync(GPUMapMode.READ);
+  const arrayBuffer = gpuReadBuffer.getMappedRange();
+  console.log(new Float32Array(arrayBuffer));
+
+})();
+
---
+++ shader/matrix.wgsl
@@ -0,0 +1,41 @@
+@group(0) @binding(0) var<storage, read> firstMatrix : array<f32>;
+@group(0) @binding(1) var<storage, read_write> secondMatrix : array<f32>;
+
+const gravParam = 0.01;
+
+@compute @workgroup_size(8)
+
+fn main(@builtin(global_invocation_id) global_id: vec3u) {
+    let index = global_id.x * 5;
+
+    // Guard against out-of-bounds work group sizes
+    if index >= u32(arrayLength(&firstMatrix)) {
+        return;
+    }
+
+    var position = vec2(firstMatrix[index], firstMatrix[index + 1]);
+    var velocity = vec2(firstMatrix[index + 2], firstMatrix[index + 3]);
+    let mass = firstMatrix[index + 4];
+
+    var acceleration = vec2(0.0, 0.0);
+    for (var i = 0; i < i32(arrayLength(&firstMatrix)); i = i + 5) {
+        if (i == i32(index)) {continue;}
+        let otherPosition = vec2(firstMatrix[i], firstMatrix[i + 1]);
+        let otherMass = firstMatrix[i + 4];
+
+        let pathBetween = vec2(otherPosition.x - position.x, otherPosition.y - position.y);
+        let direction = normalize(pathBetween);
+        let squaredDistance = pathBetween.x * pathBetween.x + pathBetween.y * pathBetween.y;
+
+        let force = gravParam * ((mass * otherMass) / squaredDistance);
+        acceleration = vec2(acceleration.x + direction.x * force, acceleration.y + direction.y * force);
+    }
+    
+    velocity = vec2(velocity.x + acceleration.x, velocity.y + acceleration.y);
+    position = vec2(position.x + velocity.x, position.y + velocity.y);
+
+    secondMatrix[index] = position.x;
+    secondMatrix[index + 1] = position.y;
+    secondMatrix[index + 2] = velocity.x;
+    secondMatrix[index + 3] = velocity.y;
+}
